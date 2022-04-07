b9fcee600168584bde2cee81c5532662803e7b51
Iván Ajenjo <ivan.ajenjo.vicente@gmail.com>
2022-04-07 21:23:34 +0200

    Initial commit

---
+++ src/main/java/com/ivajenjo/christmasapi/ChristmasapiApplication.java
@@ -0,0 +1,13 @@
+package com.ivajenjo.christmasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmasapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristmasapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivajenjo/christmasapi/ChristmasapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivajenjo.christmasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristmasapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
